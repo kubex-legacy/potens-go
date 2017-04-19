48de8661c12761af4a14847438b6d8e9a1db25d2
Brooke Bryan <brooke@bajb.net>
2017-04-19 17:25:46 +0100

    Make GaID

--- gaid.go
+++ gaid.go
@@ -3,12 +3,28 @@ package potens
 import (
 	"errors"
 	"strings"
+	"github.com/cubex/cubeutil-go"
 )
 
 var (
 	//ErrInvalidGlobalAppID invalid Global App ID
 	ErrInvalidGlobalAppID = errors.New("The Global App ID specified is invalid")
 )
+
+//MakeGaID Create a Global App ID from your vendor and application IDs
+func MakeGaID(vendor string, application string) (string, error) {
+	err := cubeutil.ValidateID(vendor)
+	if err != nil {
+		return "", err
+	}
+
+	err = cubeutil.ValidateID(application)
+	if err != nil {
+		return "", err
+	}
+
+	return vendor + "/" + application, nil
+}
 
 //SplitGaID Split a Global App ID into Vendor and App ID
 func SplitGaID(gaid string) (string, string, error) {
