2616d00037fa0508f6673ba9b8cca4c89f42492a
Brooke Bryan <brooke@bajb.net>
2017-06-23 11:36:30 +0100

    Add some quick start logging

--- quickStart.go
+++ quickStart.go
@@ -7,8 +7,12 @@ import (
 
 func QuickStartApp(name string, ident *identity.AppIdentity, def *definition.AppDefinition) (*Application) {
 	app := NewApplication(name)
+	app.Log().Info("Starting Application " + name)
 	app.FatalErr(app.SetIdentity(ident))
+	app.Log().Info("Processed Identity")
 	app.FatalErr(app.SetDefinition(def))
+	app.Log().Info("Processed Definition")
 	app.FatalErr(app.CreateServer())
+	app.Log().Info("Created gRPC Server")
 	return app
 }
