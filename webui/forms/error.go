8aa19fdfff9270c6948af3aef7da4293fedbd29b
Tom Kay <oridan82@gmail.com>
2017-07-18 11:40:16 +0100

    add WasSuccessful for easier response handling

--- webui/forms/error.go
+++
@@ -1,9 +0,0 @@
-package forms
-
-func (r *FormResponse) AddError(field string, err string) {
-	if _, ok := r.Errors[field]; !ok {
-		r.Errors[field] = []string{}
-	}
-
-	r.Errors[field] = append(r.Errors[field], err)
-}
--- webui/forms/response.go
+++ webui/forms/response.go
@@ -3,15 +3,26 @@ package forms
 import "net/http"
 
 type FormResponse struct {
-	Errors       map[string][]string
-	ToastMessage string
-	RedirectPath string
-	StatusCode   int
+	WasSuccessful bool
+	Errors        map[string][]string
+	ToastMessage  string
+	RedirectPath  string
+	StatusCode    int
 }
 
 func NewResponse() *FormResponse {
 	frmResp := &FormResponse{}
 	frmResp.Errors = make(map[string][]string)
 	frmResp.StatusCode = http.StatusOK
+	frmResp.WasSuccessful = true
 	return frmResp
+}
+
+func (r *FormResponse) AddError(field string, err string) {
+	if _, ok := r.Errors[field]; !ok {
+		r.Errors[field] = []string{}
+	}
+
+	r.Errors[field] = append(r.Errors[field], err)
+	r.WasSuccessful = false
 }
